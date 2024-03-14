7002b65c94a9b1b9d9254d3da8bc3458285f659f
ByDati <141085109+ByDati@users.noreply.github.com>
2024-03-14 13:37:35 +0000

    listo

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,4 +1,5 @@
 def juego_del_dado():
+    
     """
     Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
     Esto genera un número al azar que se le suma a la puntuación del usuario.
